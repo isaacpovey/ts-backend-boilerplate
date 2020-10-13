5747f00a48602f182d3c72abc0481a40e3902884
Isaac Povey <ipovey@skedulo.com>
2020-10-13 11:22:40 +1000

    Remove Dist

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-require('./app');
-process.title = 'ts-backend';
--- dist/routes/BaseRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseRoute = void 0;
-const Router = require("koa-router");
-exports.BaseRoute = new Router();
-exports.BaseRoute.get('/ping', async (ctx) => {
-    ctx.body = 'pong';
-});
